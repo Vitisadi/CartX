4715fc877579f9d44e9b3b9ada772f872d11f9ee
Nikul Patel <pateln14@rpi.edu>
2023-09-15 16:58:51 -0400

    added a landing home page and wrote content

---
+++ frontend/src/pages/Header.jsx
@@ -0,0 +1,11 @@
+import "../styles/index.css"
+
+const Header = () => {
+    return ( 
+        <div>
+            <h1 className="title">CartX</h1>
+        </div>
+     );
+}
+ 
+export default Header;
