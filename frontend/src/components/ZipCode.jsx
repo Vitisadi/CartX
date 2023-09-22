dacda4bc9034fb7c3326d53f080367175a3dd1ac
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2023-09-22 17:45:19 -0400

    Adde example calls to get data

--- frontend/src/components/ZipCode.jsx
+++ frontend/src/components/ZipCode.jsx
@@ -4,44 +4,66 @@ import "../styles/index.css";
 const SearchBar = () => {
   const [zipCode, setZipCode] = useState("");
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
-    try {
-      const response = await fetch("http://localhost:8080/nearbyStores", {
+    // First API call to nearbyStores
+    fetch("http://localhost:8080/nearbyStores", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ zipCode }),
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Failed to fetch data from nearbyStores");
+      }
+    })
+    .then(data1 => {
+      console.log("Received data from nearbyStores:", data1);
+    
+      // Second API call to getData
+      return fetch("http://localhost:8080/getData", {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ zipCode }),
+        body: JSON.stringify({ stores: data1.stores }),  
       });
-
+    })
+    .then(response => {
       if (response.ok) {
-        const data = await response.json();
-        console.log("Received data:", data);
+        return response.json();
       } else {
-        console.log("Failed to fetch data");
+        throw new Error("Failed to fetch data from getData");
       }
-    } catch (error) {
+    })
+    .then(data2 => {
+      console.log("Received data from getData:", data2);
+    })
+    .catch(error => {
       console.error("There was a problem with the fetch operation:", error);
-    }
+    });
   };
 
   return (
     <div className="container">
-      <form onSubmit={handleSubmit}>
-        <label>
-          Zip Code:
-          <input
-            type="text"
-            name="Zip Code"
-            value={zipCode}
-            onChange={(e) => setZipCode(e.target.value)}
-          />
-        </label>
-        <input type="submit" value="Zip Code Submit" />
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Zip Code:
+        <input
+          type="text"
+          name="Zip Code"
+          value={zipCode}
+          onChange={(e) => setZipCode(e.target.value)}
+        />
+      </label>
+      <input type="submit" value="Zip Code Submit" />
+    </form>
+  </div>
   );
 };
 
