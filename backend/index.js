e1e973962935c42e72e61eb5605e73665641e19f
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2023-09-22 17:44:41 -0400

    Updated routes to be imported from directories

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import fs from 'fs';
-import { fileURLToPath } from 'url';
+import { fileURLToPath, pathToFileURL } from 'url';
 import path from 'path';
 
 const app = express();
@@ -18,24 +18,34 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// Dynamically import route files
+// Function to recursively import routes
+const importRoutesFromDirectory = (directory) => {
+  fs.readdir(directory, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      console.error('Could not list the directory.', err);
+      process.exit(1);
+    }
+
+    files.forEach((dirent) => {
+      const fullPath = path.join(directory, dirent.name);
+      if (dirent.isDirectory()) {
+        importRoutesFromDirectory(fullPath);
+      } else if (dirent.name.endsWith('.js')) {
+        const fileURL = pathToFileURL(fullPath).href;
+        import(fileURL).then((router) => {
+          app.use('/', router.default);
+        });
+      }
+    });
+  });
+};
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const routesDirectory = path.join(__dirname, 'routes');
 
-fs.readdir(routesDirectory, (err, files) => {
-  if (err) {
-    console.error('Could not list the directory.', err);
-    process.exit(1);
-  }
-
-  files.forEach((file, index) => {
-    import(`./routes/${file}`).then((router) => {
-      app.use('/', router.default);
-    });
-  });
-});
+importRoutesFromDirectory(routesDirectory);
 
 const port = 8080;
 const server = app.listen(port, () => {
