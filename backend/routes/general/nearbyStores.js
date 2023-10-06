c3efedbbeb1b4cbf7ebf50804c99e1a1a65f9056
Robert Acebedo <robace747@gmail.com>
2023-10-06 17:43:29 -0400

    Implemented Places API Call
    API Call successfully finds nearby locations based on given location, type, and radius.

--- backend/routes/general/nearbyStores.js
+++ backend/routes/general/nearbyStores.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import dotenv from 'dotenv';
 const router = express.Router();
 
 // Get file route name (Same as file name)
@@ -14,22 +15,36 @@ router.put(`/${parsed.name}`, (req, res) => {
     return res.status(400).json({ error: 'ZipCode is required' });
   }
 
-  require('dotenv').config(); // Load environment variables from .env file
+  dotenv.config(); // Load environment variables from .env file
 
   // Access your API key
   const apiKey = process.env.API_KEY;
 
-  // Define the endpoint for the Nearby Search request
-  const endpoint = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
+ // Search Parameters
+ const location = '42.7299%2C73.6767'; // Using RPI Union as a base location to test API use
+ const radius = '10000'; // Radius in meters
+ const type = 'store' //Using grocery stores as key word
 
-  // Define your search parameters
-  const location = 'latitude,longitude'; // Replace with the coordinates of your location
-  const radius = '500'; // Radius in meters
-  //const keyword; tbd
+  // API URL for Nearby Search request
+  const apiUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=${radius}&type=${type}&key=${apiKey}`;
+  
+  fetch(apiUrl)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Network response was not ok (${response.status})`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data); // Log the JSON response to the console
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 
 
-
-
+  
+  
 
 
 
