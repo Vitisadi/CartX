190504218f3a41c5c7606bf804ce56830fccfba5
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2023-10-06 17:00:16 -0400

    Added basic frontend interface for selection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,27 @@
+import React, { useState } from "react";
 import Body from './components/Body';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import ZipCode from './components/ZipCode';
+import ItemList from './components/ItemList';
 
 function App() {
-  return (
+  const [items, setItems] = useState([]);
+  const [zipCode, setZipCode] = useState('');
 
+  const handleSubmit = () => {
+    console.log(zipCode);
+    console.log(items);
+
+    
+  };
+
+  return (
     <div>  
       <Header/>
       <Body/>
-      <ZipCode></ZipCode>
+      <ItemList items={items} setItems={setItems} />
+      <ZipCode zipCode={zipCode} setZipCode={setZipCode} handleSubmit={handleSubmit} />
       <SearchBar/>
     </div>
   );
