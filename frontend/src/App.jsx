bedfbd442e170e2a7a519938a0b420116faba926
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2023-10-06 17:13:36 -0400

    Added process zipcode and items and send to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,53 @@ import ZipCode from './components/ZipCode';
 import ItemList from './components/ItemList';
 
 function App() {
-  const [items, setItems] = useState([]);
   const [zipCode, setZipCode] = useState('');
+  const [items, setItems] = useState([]);
 
-  const handleSubmit = () => {
-    console.log(zipCode);
-    console.log(items);
-
-    
+  const handleSubmit = () => {  
+      // First API call to nearbyStores
+      fetch("http://localhost:8080/nearbyStores", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ zipCode }),
+      })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Failed to fetch data from nearbyStores");
+        }
+      })
+      .then(data1 => {
+        console.log("Received data from nearbyStores:", data1);
+      
+        // Second API call to getData
+        return fetch("http://localhost:8080/getData", {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            stores: data1.stores, 
+            items: items
+          }),  
+        });
+      })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Failed to fetch data from getData");
+        }
+      })
+      .then(data2 => {
+        console.log("Received data from getData:", data2);
+      })
+      .catch(error => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
   };
 
   return (
