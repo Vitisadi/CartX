c9097aa243e459ead2e018f59473cab9b77a6581
Nikul Patel <pateln14@rpi.edu>
2023-10-20 16:53:52 -0400

    added packages for headless

--- backend/routes/webScrapeRoutes/target_scrape.js
+++ backend/routes/webScrapeRoutes/target_scrape.js
@@ -1,22 +1,27 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+
 const router = express.Router();
 
 // Get file route name (Same as file name)
 const __filename = fileURLToPath(import.meta.url);
 const parsed = path.parse(__filename);
 
-import { Builder, By, until } from 'selenium-webdriver'; // Import necessary modules
+import { Builder, By, until } from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome.js';
+async function scrapeTargetWebsite() {
+  const item = "icecream";
+  const zipcode = "10954";
 
-async function scrapeTargetWebsite( /* pass in a parameter where you get in item and zipcode */ ) {
-
-  const item = "icecream"
-  const zipcode = "10954" // example zipcode
-
-  // Initialize a Selenium WebDriver (you should have ChromeDriver installed)
+  // Set Chrome options to run in headless mode
+  const chromeOptions = new chrome.Options();
+  // chromeOptions.addArguments('--headless');
+  chromeOptions.addArguments('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
+  // Initialize a Selenium WebDriver with Chrome options
   const driver = new Builder()
     .forBrowser('chrome')
+    .setChromeOptions(chromeOptions)
     .build();
 
   try {
@@ -33,27 +38,25 @@ async function scrapeTargetWebsite( /* pass in a parameter where you get in item
     const max_scroll_height = await driver.executeScript("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );");
 
     // Loop through the elements and extract data
-    for (let i = 1; i <= 11; i++) {
-
-      // skip the ads
-      if (i == 9){continue}
-
-      // Construct XPath for the product name
-      const xpath_name_item = `//*[@id="pageBodyContainer"]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/section[1]/div[1]/div[${i}]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]`;
-      
-      // Construct XPath for the item price
-      const xpath_name_price = `//*[@id="pageBodyContainer"]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/section[1]/div[1]/div[${i}]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/span[1]/span[1]`;
-
-      if (max_scroll_height > curr_height) {
-        await driver.executeScript(`window.scrollBy(0, ${scroll_height});`);
-        await driver.sleep(0.5); 
-        curr_height += scroll_height;
-      }
+    for (let item_number = 1; item_number <= 5; item_number++) {
+      console.log(item_number);
 
       try {
-        // Attempt to locate the element, but with a timeout of 0 milliseconds
-        const name_element = await driver.wait(until.elementLocated(By.xpath(xpath_name_item), 0));
-        const price_element = await driver.wait(until.elementLocated(By.xpath(xpath_name_price), 0));
+        // Construct XPath for the product name
+        const xpath_name_item = `//*[@id="pageBodyContainer"]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/section[1]/div[1]/div[${item_number}]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]`;
+
+        // Construct XPath for the item price
+        const xpath_name_price = `//*[@id="pageBodyContainer"]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/section[1]/div[1]/div[${item_number}]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/span[1]/span[1]`;
+
+        if (max_scroll_height > curr_height) {
+          await driver.executeScript(`window.scrollBy(0, ${scroll_height});`);
+          await driver.sleep(0.5);
+          curr_height += scroll_height;
+        }
+
+        // Attempt to locate the elements, but with a timeout of 10 seconds
+        const name_element = await driver.wait(until.elementLocated(By.xpath(xpath_name_item), 10));
+        const price_element = await driver.wait(until.elementLocated(By.xpath(xpath_name_price), 10));
 
         // Extract the data
         const item_name = await name_element.getText();
@@ -69,9 +72,12 @@ async function scrapeTargetWebsite( /* pass in a parameter where you get in item
             'item_name': decodedItemName.trim(),
             'item_price': item_price.trim(),
           });
+        } else {
+          // If either name or price is missing, skip this index
+          console.log(`Skipping product ${item_number} due to missing data.`);
         }
       } catch (error) {
-        console.log(`Error processing product ${i}: ${error}`);
+        console.log(`Error processing product ${item_number}: ${error}`);
       }
     }
 
