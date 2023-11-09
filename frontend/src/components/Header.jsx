8eeea71fa66ba3993dd83427aa8ebb04f7fd22ed
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2023-11-09 11:30:09 -0500

    Began working on the frontend header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,25 @@
-import "../styles/index.css"
+import React from 'react';
+import logo from '../assets/cartx.png';
+import "../styles/header.css"
 
 const Header = () => {
-    return ( 
-        <div>
-            <h1 className="title">CartX</h1>
-        </div>
-     );
+    return (
+        <header className="site-header">
+            <div class="logo-container">
+                <img src={logo} alt="Logo" className="header-logo" />
+            </div>
+            <div className="search-container">
+                <form className="search-form">
+                    <span className="search-icon">&#x1F50D;</span>
+                    <input type="text" placeholder="Search..." className="search-input" />
+                    <button type="submit" className="search-button">+</button>
+                </form>
+            </div>
+            <div class="address-container">
+
+            </div>
+        </header>
+    );
 }
  
 export default Header;
