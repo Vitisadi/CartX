ca3d0c1682c1529a32a9e965f475cec3203e24b2
Robert Acebedo <robace747@gmail.com>
2023-11-10 17:43:24 -0500

    Implemented header
    Implemented a design for the header.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import logo from '../assets/cartx.png';
+import { SlMagnifier, SlMenu } from "react-icons/sl";
+import {TiShoppingCart, TiLocation} from "react-icons/ti";
 import "../styles/header.css"
 
 const Header = () => {
     return (
         <header className="site-header">
-            <div class="logo-container">
-                <img src={logo} alt="Logo" className="header-logo" />
+            <div class="left">
+                <button className="hamburger-button">
+                    <SlMenu />
+                </button>
+                <img src={logo} alt="Logo" className="header-logo" />             
             </div>
-            <div className="search-container">
+            <div className="middle">
                 <form className="search-form">
-                    <span className="search-icon">&#x1F50D;</span>
-                    <input type="text" placeholder="Search..." className="search-input" />
-                    <button type="submit" className="search-button">+</button>
+                    <span className="magnifying-glass"><SlMagnifier /></span>
+                    <input type="text" placeholder="Search..." className="search-bar" />
                 </form>
-            </div>
-            <div class="address-container">
 
+            </div>
+            <div class="right">
+                <button className="input-address">
+                    <TiLocation size={30} />
+                </button>
+                <button className="view-cart">
+                    <TiShoppingCart size={30}/>
+                </button>
             </div>
         </header>
     );
